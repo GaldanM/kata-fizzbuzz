e1288f72357ab2482dd8c17ccb3b06bdeb97b724
Galdan Moulinneuf <14978190+GaldanM@users.noreply.github.com>
2021-06-01 16:42:17 +0200

    Refactor tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,39 +1,25 @@
 import fizzbuzz from "../src/fizzbuzz";
 
+function testFizzBuzz(input: number, expectedOutput: number | string) {
+  const output = fizzbuzz(input);
+
+  expect(output).toStrictEqual(expectedOutput);
+}
+
 describe("fizzBuzz", () => {
   it("number 1 should return 1", () => {
-    const input = 1;
-
-    const output = fizzbuzz(input);
-
-    expect(output).toStrictEqual(1);
+    testFizzBuzz(1, 1);
   });
   it("number 2 should return 2", () => {
-    const input = 2;
-
-    const output = fizzbuzz(input);
-
-    expect(output).toStrictEqual(2);
+    testFizzBuzz(2, 2);
   });
   it("number 3 should return Fizz", () => {
-    const input = 3;
-
-    const output = fizzbuzz(input);
-
-    expect(output).toStrictEqual("Fizz");
+    testFizzBuzz(3, "Fizz");
   });
   it("number 6 should return Fizz", () => {
-    const input = 6;
-
-    const output = fizzbuzz(input);
-
-    expect(output).toStrictEqual("Fizz");
+    testFizzBuzz(6, "Fizz");
   });
   it("number 9 should return Fizz", () => {
-    const input = 9;
-
-    const output = fizzbuzz(input);
-
-    expect(output).toStrictEqual("Fizz");
+    testFizzBuzz(9, "Fizz");
   });
 });
